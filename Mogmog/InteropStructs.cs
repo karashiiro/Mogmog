7f31de405fdf8615640a425cf71b15b80f40f664
karashiiro <49822414+karashiiro@users.noreply.github.com>
2020-04-08 19:04:28 -0700

    Implement IEquatable on structs to quiet the linter

--- Mogmog/InteropStructs.cs
+++ Mogmog/InteropStructs.cs
@@ -1,16 +1,27 @@
 ﻿using Mogmog.Protos;
+using System;
 
 namespace Mogmog
 {
-    public struct ChatMessageInterop
+    public struct ChatMessageInterop : IEquatable<ChatMessageInterop>
     {
         public ChatMessage Message { get; set; }
         public int ChannelId { get; set; }
+
+        public bool Equals(ChatMessageInterop other)
+        {
+            return Message == other.Message && ChannelId == other.ChannelId;
+        }
     }
 
-    public struct GenericInterop
+    public struct GenericInterop : IEquatable<GenericInterop>
     {
         public string Command { get; set; }
         public string Arg { get; set; }
+
+        public bool Equals(GenericInterop other)
+        {
+            return Command == other.Command && Arg == other.Arg;
+        }
     }
 }
